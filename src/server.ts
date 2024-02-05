7a87db88b71ba09879c01148c107e7326ce2d853
mickalala <mickalarissa01@gmail.com>
2024-02-05 17:04:32 -0300

    chore: adjust code for deploy on render

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
 
-const PORT = 5000;
+const port = process.env.PORT || 5000
 
-    app.listen(PORT, () => console.log(`server is up and running on port ${PORT}`));
+    app.listen(port, () => console.log(`server is up and running on port ${port}`));
